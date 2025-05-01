7fbd37c9cb91b22cba7c8f15c6a14bc17e9ca94f
Swapnxll <130583218+Swapnxll@users.noreply.github.com>
2025-05-01 22:38:03 +0530

    CRUD done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import express from "express";
 import cors from "cors"; //Enables Cross-Origin Resource Sharing (CORS) in your Node.js application
 import dotenv from "dotenv";
 import pool from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
+import errorHandling from "./middleware/errorHandler.js";
+import createUserTable from "./data/createUserTable.js";
 dotenv.config();
 
 const app = express();
@@ -12,12 +15,21 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
 
+// Error handling middleware
+app.use(errorHandling);
+
+//Create table before starting server
+createUserTable();
+
 //test postgres
 app.get("/", async (req, res) => {
   const result = await pool.query("SELECT current_database()");
 
   res.send(`the db name is :${result.rows[0].current_database}`);
 });
+
+// Routes
+app.use("/api", userRoutes);
 
 // Server running
 app.listen(port, () => {
